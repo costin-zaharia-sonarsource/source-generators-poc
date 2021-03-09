6c86a75b68799476008206f83d74aedf30b42115
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-03-09 12:35:50 +0100

    Add first working sample

---
+++ SourceGeneratorPOC.Generators/SourceGenerator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+namespace SourceGeneratorPOC
+{
+    [Generator]
+    public class SourceGenerator : ISourceGenerator
+    {
+        private const string MessagesCode = @"namespace Generated
+{
+    public static class Messages
+    {
+        public const string Hello = ""Hello from generated code!"";
+    }
+}
+";
+        public void Execute(GeneratorExecutionContext context)
+        {
+            context.AddSource("Greetings", SourceText.From(MessagesCode, Encoding.UTF8));
+        }
+
+        public void Initialize(GeneratorInitializationContext context) { }
+    }
+}
---
+++ SourceGeneratorPOC/Program.cs
@@ -0,0 +1,5 @@
+﻿using System;
+using Generated;
+
+Console.WriteLine("Hello from main project!");
+Console.WriteLine(Messages.Hello);
