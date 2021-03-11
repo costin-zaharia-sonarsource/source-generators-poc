e75f53badaaa0d0e002217921fd29c78d23b8ab4
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-03-11 17:18:16 +0100

    Add code which is security sensitive

--- SourceGeneratorPOC.Generators/SourceGenerator.cs
+++ SourceGeneratorPOC.Generators/SourceGenerator.cs
@@ -7,19 +7,38 @@ namespace SourceGeneratorPOC
     [Generator]
     public class SourceGenerator : ISourceGenerator
     {
-        private const string MessagesCode = @"namespace Generated
+        private const string MessagesCode = @"
+namespace Generated
 {
     public static class Messages
     {
         public const string Hello = ""Hello from generated code!"";
 
-        private static void UnusedMethod() { } // Issue raised here
+        private static void UnusedMethod() { } // sonar-dotnet issue raised here
     }
 }
 ";
+
+        private const string WithSecurityIssue = @"
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace Generated
+{
+    public partial class Unsafe : Controller
+    {
+        public void Index(string tainted)
+        {
+            new Regex(tainted).ToString(); // sonar-security issue should be raised here
+        }
+    }
+}
+";
+
         public void Execute(GeneratorExecutionContext context)
         {
             context.AddSource("Greetings", SourceText.From(MessagesCode, Encoding.UTF8));
+            context.AddSource("Unsafe", SourceText.From(WithSecurityIssue, Encoding.UTF8));
         }
 
         public void Initialize(GeneratorInitializationContext context) { /* Nothing to do here */ }
